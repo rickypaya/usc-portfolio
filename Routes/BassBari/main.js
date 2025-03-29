1dd832de4ce1f3f9f7b6d707245b6065a7ffe35f
Ricky Payares <rpayaresswe@gmail.com>
2025-03-29 14:46:13 -0400

    working on michael

---
+++ Routes/BassBari/main.js
@@ -0,0 +1,190 @@
+import * as THREE from 'three';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+
+//smooth scrolling & scroll trigger
+const lenis = new Lenis();
+lenis.on("scroll", ScrollTrigger.update);
+gsap.ticker.add((time) => {
+    lenis.raf(time*1000);
+});
+gsap.ticker.lagSmoothing(0);
+
+//setup
+//camera!
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x3D0C02);
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true,
+});
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setClearColor(0xffffff, 1);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.physicallyCorrectLights = true;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 3;
+document.querySelector("#three-container").appendChild(renderer.domElement);
+
+//window resize responsiveness
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    basicAnimate();
+}, false);
+
+//loaders
+let model;
+const loader = new GLTFLoader();
+loader.load(
+    './models/Theater-simple.glb',
+    function (gltf) {
+        model = gltf.scene;
+        console.log(model);
+        model.traverse((node) => {
+            if(node.isMesh) {
+                if(node.material) {
+                    node.material.metalness = 0;
+                    node.material.roughness = 1.0;
+                    node.material.envMapIntensity = 1.5;
+                }
+                node.castShadow = true;
+                node.recieveShadow = true;
+            }
+        });
+
+        const box = new THREE.Box3().setFromObject(model);
+        let center = box.getCenter(new THREE.Vector3());
+        model.position.sub(center);
+        scene.add(model);
+
+        const size = box.getSize(new THREE.Vector3());
+        const maxDim = Math.max(size.x, size.y, size.z);
+        camera.position.z = maxDim * 1.2;
+        camera.lookAt(0, 0, 0);
+
+        model.scale.set(0, 0, 0);
+        playInitialAnimation();
+
+        cancelAnimationFrame(basicAnimate);
+        animate();
+        
+    },
+    function (xhr) {
+    },
+    undefined, function (error) {
+        console.log("Error: " + error)
+    }
+);
+
+const floatAmp = 0.1;
+const floatSpeed = 1.5;
+const rotationSpeed = 0.3;
+let isFloating = true;
+let currentScroll = 0;
+const stickyHeight = window.innerHeight;
+const scheduleSection = document.querySelector("#schedule");
+const schedulePos = scheduleSection.offsetTop;
+
+function playInitialAnimation() {
+    //initial animation to scale in theater
+    if(model) {
+        setTimeout(() => {
+            gsap.to(model.scale, {
+                x: 1, y: 1, z: 1,
+                duration: 1,
+                ease: "power2.out", 
+            });
+        }, 3000);
+    }
+}
+
+ScrollTrigger.create({
+    //animation for rescaling theater after dismissing
+    trigger: "body",
+    start: "top top",
+    end: "top -10",
+    onEnterBack: () => {
+        if(model) {
+            gsap.to(model.scale, {
+                x: 1, y: 1, z: 1,
+                duration: 1,
+                ease: "power2.out"
+            });
+            isFloating = true;
+        }
+    }
+})
+
+ScrollTrigger.create({
+    //trigger to spin and scale down theater
+    trigger: "#schedule",
+    start: "top top",
+    end: `${stickyHeight}px`,
+    pin: true,
+    onEnter: () => {
+        if(model) {
+            isFloating = false;
+            model.position.y = -1;
+
+            gsap.to(model.rotation, {
+                y: model.rotation.y + Math.PI * 2,
+                duration: 1,
+                ease: "power2.inOut",
+                onComplete: () => {
+                    gsap.to(model.scale, {
+                        x: 0, y: 0, z: 0,
+                        duration: 0.5,
+                        ease: "power2.in"
+                    })
+                }
+            })
+        }
+    }
+})
+
+lenis.on("scroll", (e) => {
+    currentScroll = e.scroll;
+})
+
+function animate() {
+    if(model){
+        //floating animation
+        if(isFloating){
+            const floatOffset = Math.sin(Date.now() * 0.001 * floatSpeed) * floatAmp -1;
+            model.position.y = floatOffset;
+        }
+    }
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+
+//lights!
+const ambientLight = new THREE.AmbientLight(0xffffff, 3);
+scene.add(ambientLight);
+
+const mainLight = new THREE.DirectionalLight(0xffffff, 2);
+mainLight.position.set(0, 10, 0);
+scene.add(mainLight);
+
+const spotLight = new THREE.SpotLight(0xEFDECD, 40, 5, -Math.PI/16);
+spotLight.position.set(0, 1.8, 0);
+spotLight.castShadow = true;
+scene.add(spotLight);
+
+const pointLight = new THREE.PointLight(0xCC7F3B, 30, 30, 1);
+pointLight.position.set(0, 1, 3);
+pointLight.castShadow = true;
+scene.add(pointLight);
+
+
+//action!
+function basicAnimate() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(basicAnimate);
+}
+
+basicAnimate();
