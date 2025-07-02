14c2d3d3b6e489263889477caa940676a6a29c63
Ricky Payares <rpayaresswe@gmail.com>
2025-07-01 22:55:45 -0400

    vite entry

--- fulcrum/vite.config.js
+++ fulcrum/vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: "./fulcrum", // Set the root directory to /fulcrum
-  build: {
-    rollupOptions: {
-      input: "./fulcrum/index.html", // Specify the entry HTML file
-    },
-  },
+  root: "../fulcrum", // Set the root directory to /fulcrum
 });
